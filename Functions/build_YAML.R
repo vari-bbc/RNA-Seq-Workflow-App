61f9b8e266b8c1751d23d23330da390942ace37b
Ian Beddows <ian.beddows@vai.org>
2026-05-01 11:41:34 -0400

    added build_YAML.R based on species_name input from globals and the master_config.yaml

---
+++ Functions/build_YAML.R
@@ -0,0 +1,45 @@
+#' Build a YAML file for RNAseq Workflow Shiny App.
+#'
+#' This is the description.
+#'
+#' These are further details.
+#'
+#'
+#' @param ref_genome_version reference genome version to use 
+#' @param species_name species to use, e.g. human, mouse, fly.
+#'
+#' @export
+#'
+#' @examples
+#' add_numbers(1, 2) ## returns 3
+#'
+build_YAML <- function(
+    ref_genome_version = '2026-02-12_15.29.54_v23', 
+    species_name = 'human',
+    FDR
+) {
+  
+  if (!requireNamespace("yaml", quietly = TRUE)) {
+    stop("Package 'yaml' is required. Install with: install.packages('yaml')")
+  }
+  
+  # first import the template master YAML
+  master <- yaml::read_yaml('/Users/ian.beddows/Desktop/RNA-Seq-Workflow-App/Necessary_Files/master_config.yaml')
+  
+  # substitute in ref_genome_version for [[species_name]]
+  master[[species_name]]$ref <- lapply(master[[species_name]]$ref, gsub, pattern = "VERSION", replacement = ref_genome_version)
+  
+  # make additional modifications to YAML from input args
+  # < to do >
+  
+  # write this out along with the other unchanged yaml
+  write_yaml(
+    c(
+      master[[species_name]], 
+      master[["unchanged"]]
+    ),
+    "output_config.yaml"
+  )
+  
+
+}
