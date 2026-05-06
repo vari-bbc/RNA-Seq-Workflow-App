aa54f65fd9850da92647d4cc90e7f0e3957b6d61
Daniella DeWeerd <51303500+DaniellaDeWeerd@users.noreply.github.com>
2026-05-06 11:02:17 -0400

    Some Changes Overall
    Relative paths again, Rearranging tabs, Updating names, Updating way of loading input

--- app.R
+++ app.R
@@ -13,17 +13,16 @@ pacman::p_load(shiny,bslib,shinyjs,shinyWidgets,bsicons,plotly,DT,readr,
 
 
 ## 2.0 Load Basics ----
-basePath <<- "/varidata/research/projects/bbc/research/BBC_20260415_RNA_Seq_App_VBCS-1647/RNA-Seq-Workflow-App/"
 functionFolderPath <<- "Functions"
-source(paste0(basePath,functionFolderPath,"/Updated Shiny RMD Standards.R"))
-sourceFunctions(functionFolderPath)
+source(paste0(functionFolderPath,"/Updated Shiny RMD Standards.R"))
+sourceFunctions(paste0(basePath,functionFolderPath))
 
 
-## 3.0 Universal Vars ----
+## 3.0 Universal Vars ----fr
 # App Name here:
 appName <<- "RNA Seq Workflow Starter"
 # Necessary Files here:
-template <<- read_excel(paste0(basePath,"Necessary Files/SampleTemplate.xlsx"), sheet = 1)
+template <<- read_excel(paste0("Necessary Files/SampleTemplate.xlsx"), sheet = 1)
 # Root Dir for Folder Selection:
 rootDir <<- c(Home = "~", "HPC Primary" = "~/../../primary", "HPC Secondary" = "~/../../secondary")
 restrictDir <<- c("afs","bin","cloudstorage","cm","dev","etc","legacy","lib",
@@ -39,11 +38,17 @@ ui <- UINav(
 
   ## 1.0 Input ----
   singleTab("Input File",
-    navDownload("templateDownload", "Download a Template", 
+    navDownload("templateDownload", "Download a Template",
                 tooltipText = "Required columns: Conditions, Sample ID, ..."),
     navUpload("sampleUpload", "Upload your Sample Sheet", "Single"),
-    navButton("uploadButton", "Load the Samples"),
-    navOutputText("errorText")
+    shinyDirButton("inputPathSelect","Select Fastq Input Folder",
+                   "Please select a folder", viewtype = "icon"),
+    shinyDirButton("outputPathSelect","Select Workflow Output Folder",
+                   "Please select a folder", viewtype = "icon"),
+    navButton("checkFiles", "Check Files and Folders"),
+    navOutputText("errorText"),
+    navOutputText("inputErrorText"),
+    navOutputText("outputErrorText")
   ),
   
   ## 2.0 Input ----
@@ -57,25 +62,14 @@ ui <- UINav(
               theChoices = c("Paired End","Single End")),
     navCheckbox("visBigWig","Run VisBigWig", "True"),
     navCheckbox("rSeqC","Run rSeqC", "True"),
-    navSelect("relevantComps", "Select Comparisons", "Multi", "Locked"),
-    actionButton("createFiles","Create Files for Workflow"),
+    actionButton("compileConfig","Compile Config"),
     hr(),
-    navDownload("workflowFiles","Download a Zip of the Workflow Files")
+    navDownload("workflowFiles","Download a Zip of the Workflow Input Files")
   ),
-  
-  ## 3.0 Run Workflow ----
-  singleTab("Run Workflow",
-    shinyDirButton("inputPathSelect","Select Fastq Input Folder",
-                   "Please select a folder", viewtype = "icon"),
-    shinyDirButton("outputPathSelect","Select Workflow Output Folder",
-                   "Please select a folder", viewtype = "icon"),
-    navButton("runWorkflow","Run the Whole Workflow", 
-              tooltipText = "You will receive an email once it is done with a 
-              link to the output location"),
-    navOutputText("inputErrorText"),
-    navOutputText("outputErrorText"),
-    navOutputText("notificationText")
+  singleTab("Select Comparisons and Run",
+    navSelect("relevantComps", "Select Comparisons", "Multi", "Locked")
   )
+  
 
 )
 
@@ -89,15 +83,6 @@ server <- function(session, input, output) {
   # Any variables here that need to carry across app sections, but should be local to the user
   # Keep all variables inside a list to help with debugging later
   globals <- reactiveValues(
-    datasets = list(
-      configSettings = data.frame(
-        ref_genome_version = NA,
-        speciesSelect = NA
-        # And so on for every part we want changable
-      ),
-      unitsTSV = NULL,
-      comparisonsTSV = NULL
-    ),
     checks = list(
       inputDirCheck = F,
       outputDirCheck = F,
@@ -112,13 +97,13 @@ server <- function(session, input, output) {
     startSection("Run on Start")
     
     # Deactivate buttons that need other things to function
-    # deactivateItems(c("createFiles","runWorkflow"))
+    # deactivateItems(c("compileConfig","compileConfig"))
 
     # Deactivate downloads that need other things to function
     deactivateItems(c("workflowFiles"))
     
     #Unnecessary for final app, actually used elsewhere
-    output$errorText <- renderText({ "Text here will display if selected file path is inaccessible" })
+    output$errorText <- renderText({ "Text here will display if sample sheet is proper" })
     
     endSection("Run on Start")
   })
@@ -142,6 +127,7 @@ server <- function(session, input, output) {
   observeEvent(input$inputPathSelect,{
     inputDir <- parseDirPath(rootDir,input$inputPathSelect)
     
+    
     # Check if selected directory is readable
     
     # If it is:
@@ -153,7 +139,7 @@ server <- function(session, input, output) {
     
     # Check if workflow is runnable
     if (globals$checks$inputDirCheck & globals$checks$outputDirCheck & globals$checks$filesCheck){
-      activateItems(c("runWorkflow"))
+      activateItems(c("compileConfig"))
     }
   })
   
@@ -173,25 +159,26 @@ server <- function(session, input, output) {
     
     # Check if workflow is runnable
     if (globals$checks$inputDirCheck & globals$checks$outputDirCheck & globals$checks$filesCheck){
-      activateItems(c("runWorkflow"))
+      activateItems(c("compileConfig"))
     }
   })
   
   
-  ## 5.0 Initial Sample Upload ----
-  observeEvent(input$uploadButton, {
-    startSection("Initial sample upload")
+  ## 5.0 Check Files ----
+  observeEvent(input$checkFiles, {
+    startSection("Check files")
     
     ## Inputs
     inputFilePath <- input$sampleUpload$datapath
+    outputDir <- parseDirPath(rootDir,input$outputPathSelect)
     
     
     ## Read in xlsx first page
     inputFile <- read_xlsx(inputFilePath, sheet = 1)
     
-    # Function to pull apart file
+    ## Function to pull apart input file
     
-    # Should get all conditions
+    # Function should result in condition list
     theConditions <- c("condition1","condition2","condition3")
     
 
@@ -199,119 +186,89 @@ server <- function(session, input, output) {
     # activate downloads/buttons, set up downloads, deactivate downloads/buttons,
     # update inputs/outputs
     
-    activateItems(c("createFiles"))
+    # Check if input folder, output folder, and fastq files in input folder are good
+    # If not update the corresponding error messages
+    
+    # If everything is good, unlock the run workflow button
+    if (globals$checks$inputDirCheck & globals$checks$outputDirCheck & globals$checks$filesCheck){
+      activateItems(c("compileConfig"))
+      
+      # Interact with command line here
+      # Copy github folder to outputDir
+      # Sim link fastq files to proper github folder
+      
+    }
     
     # By default, select all conditions
     updateSelectizeInput(session, "relevantComps", choices = theConditions, 
                          selected = theConditions)
     
-    endSection("Initial sample upload")
+    endSection("Check files")
   })
   
   
-  ## 6.0 Update Config Options ----
-  # All user inputs to config saved here (also any checks we want for inputs should be here)
-  observeEvent(input$refVersions,{
-    globals$datasets$configSettings$ref_genome_version <- input$refVersions
-  }, ignoreInit = TRUE)
-  observeEvent(input$speciesSelect,{
-    globals$datasets$configSettings$speciesSelect <- input$speciesSelect
-  }, ignoreInit = TRUE)
-  
-  
-  ## 7.0 Create Workflow Files ----
-  observeEvent(input$createFiles, {
+  ## 6.0 Create Workflow Files ----
+  observeEvent(input$compileConfig, {
     startSection("Create workflow files")
     
-    ## Load globals
-    configSettings <- globals$datasets$configSettings
-    
-    
-    ## print config options -- testing --
-    print(paste("Config option PE_or_SE",as.character(input$PE_or_SE),"\n"))
-    
-    ## create the config.YAML
-    build_YAML(
-      ref_genome_version = as.character(configSettings$ref_genome_version),
-      species_name       = as.character(configSettings$speciesSelect),
-      fdrCutoff          = as.numeric(configSettings$fdrCutoff), # numeric
-      PE_or_SE           = as.character(configSettings$pairedSingle)
-    )
-    showNotification("Files created!", type = "message")
-    
-    ## Inputs
-    # Load all non config user inputs here
+    ## Load inputs
+    refVersions <- input$refVersions
+    speciesSelect <- input$speciesSelect
+    fdrCutoff <- input$fdrCutoff
+    pairedSingle <- input$pairedSingle
+    visBigWig <- input$visBigWig
+    rSeqC <- input$rSeqC
+    relevantComps <- input$relevantComps
     
     
     ## Check that there are a minimum of 2 conditions selected first!
     
-    ## Create comparisons dataframe from selections
-    
-    ## Create units dataframe from selections
-
-    ## App interactions
-    
-    globals$checks$filesCheck <- T # ? what is this doing/checking
-    
-    # Check if workflow is runnable
+    ## Check that the output folder, input folder, and fastq files are good
     if (globals$checks$inputDirCheck & globals$checks$outputDirCheck & globals$checks$filesCheck){
-      activateItems(c("runWorkflow"))
+      ## Create config file into output directory
+      
+      # print config options -- testing --
+      print(paste("Config option PE_or_SE",as.character(input$pairedSingle),"\n"))
+      
+      ## create the config.YAML
+      build_YAML(
+        ref_genome_version = as.character(refVersions),
+        species_name       = as.character(speciesSelect),
+        fdrCutoff          = as.numeric(fdrCutoff), # numeric
+        PE_or_SE           = as.character(pairedSingle)
+      )
+      showNotification("Files created!", type = "message")
+      
+      
+      ## Create comparisons dataframe from selections
+    
+      
+      ## Create units dataframe from selections
+      
+      
+      ## App interactions
+      
+      # Create zip folder of config file, unit file, and comparison file
+      activateItems(c("workflowFiles"))
+      output$workflowFiles <- downloadHandler(
+        filename = function() {
+          "RNA Seq Workflow Files.zip"
+        },
+        content = function(file) {
+          disable("workflowFiles")
+          # Make config file (should be a function to use again later)
+          
+          # Make other two dataframes into files
+          
+          # Combine all three files into one zip
+          
+          print("Done Downloading zip")
+        }
+      )
+      
     }
     
-    # Create zip folder of config file, unit file, and comparison file
-    activateItems(c("workflowFiles"))
-    output$workflowFiles <- downloadHandler(
-      filename = function() {
-        "RNA Seq Workflow Files.zip"
-      },
-      content = function(file) {
-        disable("workflowFiles")
-        # Make config file (should be a function to use again later)
-        
-        # Make other two dataframes into files
-        
-        # Combine all three files into one zip
-        
-        print("Done Downloading zip")
-      }
-    )
-
-    ## Save globals
-    # Save all files here for use in running actual workflow
-    # globals$datasets$unitsTSV <- unitsTSV
-    # globals$datasets$comparisionsTSV <- comparisionsTSV 
-    
-    
     endSection("Create workflow files")
-  })
-  
-  ## 8.0 Run Workflow ----
-  observeEvent(input$runWorkflow, {
-    startSection("Run workflow")
-    
-    ## Load globals
-    configSettings <- globals$datasets$configSettings
-    unitsTSV <- globals$datasets$unitsTSV
-    comparisionsTSV <- globals$datasets$comparisionsTSV
-    
-    
-    ## Inputs
-    inputDir <- parseDirPath(rootDir,input$inputPathSelect)
-    outputDir <- parseDirPath(rootDir,input$outputPathSelect)
-    
-    
-    ## Make and save units to input dir
-    
-    ## Make and save comparisons to input dir
-    
-    ## Check proper fastq files are in input dir
-    
-    ## Run command line prompt to do the workflow
-    
-    ## update notificationText
-    output$notificationText <- renderText({ "The workflow has started running. You will recieve an email when it is donw." })
-    
-    endSection("Run workflow")
   })
   
 }
